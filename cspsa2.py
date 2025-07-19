a84c25963a13b911c7eb8c16e3ccca791ed74178
randomtofree <technologyhomies@gmail.com>
2025-07-19 12:38:14 +0800

    fig3 ok but without continuous version

--- cspsa2.py
+++ cspsa2.py
@@ -48,6 +48,42 @@ def run_instrumental_cspsa_simulation(config, run_id=0):
     history.append(calculate_instrumental_cspsa_violation(initial_state, params, photon_num, variation, uncertainty))
     return history
 
+def run_instrumental_continuous_cspsa_simulation(config, run_id=0):
+    if run_id > 0 and run_id % 10 == 0:
+        print(f"  - [CSPSA] Starting Trial {run_id + 1}...")
+
+    initial_state = config['state']
+    iterations = config['iterations']
+    hparams = config['hparams']['cspsa']
+    a, s, b, r = hparams['a'], hparams['s'], hparams['b'], hparams['r']
+    photon_num = config['photon_num']
+    variation = config['state_variation']
+    uncertainty = config['uncertainty']
+
+    params = np.random.uniform(-1, 1, 10) + 1j * np.random.uniform(-1, 1, 10)
+    params /= np.linalg.norm(params)
+    history = []
+
+    for k in range(1, iterations + 1):
+        c_k = b / (k + 1.0) ** r
+        a_k = a / (k + 1.0) ** s
+
+        history.append(calculate_instrumental_cspsa_violation(initial_state, params, photon_num, variation, uncertainty))
+        delta = np.random.choice([1, -1, 1j, -1j], size=10)
+        params_plus = params + c_k * delta
+        params_minus = params - c_k * delta
+
+        val_plus = calculate_instrumental_cspsa_violation(initial_state, params_plus, photon_num, variation, uncertainty)
+        val_minus = calculate_instrumental_cspsa_violation(initial_state, params_minus, photon_num, variation, uncertainty)
+
+        gradient = (val_plus - val_minus) / (2 * c_k * np.conj(delta))
+        params += a_k * gradient
+        params /= np.linalg.norm(params)
+    #调试代码
+
+    history.append(calculate_instrumental_cspsa_violation(initial_state, params, photon_num, variation, uncertainty))
+    return history
+
 
 def run_chsh_cspsa_simulation(config, run_id=0):
     if run_id > 0 and run_id % 20 == 0:
